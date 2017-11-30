3e468c6c55c52b45b221bb4c1c54f4ff3d78bbb5
Ben Gubler <nebrelbug@gmail.com>
2017-11-29 17:14:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+//th stands for to hash
+
+function hashIt(val) {
+  var hv = val.toString();
+  
+  return hv;
+}
+
 function outputHash (th) {
+  var hashOut = hashIt(th);
+  
+  document.getElementById("output").innerHTML = hashOut;
 }
