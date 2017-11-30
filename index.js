84bcc57e7bb51778f674ef642d2f208dae45956f
Ben Gubler <nebrelbug@gmail.com>
2017-11-29 17:40:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function hashIt(val) {
   for (i = 0; i < hv.length + 1; i++) { 
     hArray.push(hv.charCodeAt(i).toString(2));
   }
-  for (j = 0; j <hArray.length ; j++) {\
+  for (j = 0; j <hArray.length ; j++) {
     if (hArray[j+2] !== undefined && hArray[j+2]!== null) {
     nArray.push(hArray[j] + hArray[j+2]);
     } else {
