00545204440c80c6a21bab9d50e2aa78d7473211
Ben Gubler <nebrelbug@gmail.com>
2017-11-29 17:37:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,19 @@
 function hashIt(val) {
   var hv = val.toString();
   var hArray = [];
+  var nArray = [];
   for (i = 0; i < hv.length + 1; i++) { 
-    hArray.push(hv.charCodeAt(i -1).toString(2));
+    hArray.push(hv.charCodeAt(i).toString(2));
   }
-  var hv = hArray.join('-');
+  for (j = 0; j <hArray.length ; j++) {\
+    if (hArray[j+2] !== undefined && hArray[j+2]!== null) {
+    nArray.push(hArray[j] + hArray[j+2]);
+    } else {
+      nArray.push(hArray[j]);
+    }
+  }
+  
+  var hv = nArray.join('-');
   
   return hv;
 }
